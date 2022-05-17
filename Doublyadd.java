11ddf306d750258c32ed8f9bff5f4662062a9a67
Nalan <105509330+Nalan-R@users.noreply.github.com>
2022-05-16 21:29:41 -0700

    Add files via upload

--- Doublyadd.java
+++ Doublyadd.java
@@ -1,43 +1,36 @@
-package linkledlist;
+package Linked_list;
 
-class Node2{
-	int data;
-	Node2 previous,next;
-	
-}
 public class Doublyadd {
-	Node2 head;
+	int data;
+	Node head;
 	
-	public void add(int val) {
-		Node2 n=new Node2();
-		n.data=val;
-		
-		if(head==null) {
-			n.previous=null;
-			n.next=null;
-			head=n;	
-		}
+	public void add(int value){
+		Node newnode=new Node();
+		newnode.data=value;
+		newnode.previous=null;
+
+		if(head == null) 
+			head=newnode;
 		else {
-			n.previous=null;
-		    n.next=head;
-		    head.previous=n;
-		    head=n;
+			head.previous=newnode;
+		    newnode.previous=null;
+		    head=newnode;
 		}
-	}	
-		public void print() {
-			Node2 temp=head;
-			while(temp!=null) {
-				System.out.println(temp.data);
-				temp=temp.next;
-			}
+	}
+	
+	public void print() {
+		Node temp=head;
+		while(temp!=null) {
+			System.out.println(temp.data);
+			temp=temp.next;
 		}
+	}
+	public static void main(String args[]) {
+		Doublyadd list=new Doublyadd();
 		
-		public static void main(String args[]) {
-			Doublyadd list=new Doublyadd();
-			
-			list.add(10);
-			list.add(20);
-			list.add(30);
-			list.print();
-		}
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.print();
+	}
 }
